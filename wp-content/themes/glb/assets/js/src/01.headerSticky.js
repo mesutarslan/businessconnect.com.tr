182869f668673d2e8d27df92141e5d7711a2e91e
mesutarslan <mesutarsln@gmail.com>
2022-05-07 01:10:08 +0300

    v1

--- wp-content/themes/glb/assets/js/src/01.headerSticky.js
+++
@@ -1,7 +0,0 @@
-$.headerSticky = function() {
-	$win.on( 'scroll load', function() {
-		$win.scrollTop() > $( '#site-header' ).outerHeight()
-			? $( '.site-header-sticky' ).addClass( 'active' )
-			: $( '.site-header-sticky' ).removeClass( 'active' );
-	} );
-};
