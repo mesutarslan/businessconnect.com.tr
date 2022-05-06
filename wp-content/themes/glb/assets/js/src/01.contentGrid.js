995689664bb1c8ef96b12bf0df32e774341484dc
mesutarslan <mesutarsln@gmail.com>
2022-05-06 15:07:15 +0300

    v1

--- wp-content/themes/glb/assets/js/src/01.contentGrid.js
+++
@@ -1,1 +0,0 @@
-$.fn['contentGrid']=function(){return this.each(function(){var $this=$(this);$this.imagesLoaded().then(function(){$this.isotope($.extend({},$this.data('grid'),{layoutMode:'packery',percentPosition:true}));});});};
