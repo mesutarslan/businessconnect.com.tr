df11e3bb689c6390c2e5e3087fdde6752c984f4a
mesutarslan <mesutarsln@gmail.com>
2022-05-06 14:54:07 +0300

    v1

---
+++ wp-content/themes/glb/assets/js/src/01.contentGrid.js
@@ -0,0 +1,1 @@
+$.fn['contentGrid']=function(){return this.each(function(){var $this=$(this);$this.imagesLoaded().then(function(){$this.isotope($.extend({},$this.data('grid'),{layoutMode:'packery',percentPosition:true}));});});};
---
+++ wp-content/themes/glb/assets/js/src/01.headerSticky.js
@@ -0,0 +1,1 @@
+$.headerSticky=function(){$win.on('scroll load',function(){$win.scrollTop()>$('#site-header').outerHeight()?$('.site-header-sticky').addClass('active'):$('.site-header-sticky').removeClass('active');});};
