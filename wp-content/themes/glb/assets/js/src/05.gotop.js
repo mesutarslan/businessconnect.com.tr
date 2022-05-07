08615a9ed42a4cb1aa86a5125e34661e27dde69a
mesutarslan <mesutarsln@gmail.com>
2022-05-07 17:23:01 +0300

    v1

--- wp-content/themes/glb/assets/js/src/05.gotop.js
+++
@@ -1,1 +0,0 @@
-$.gotop=function(){$('.go-to-top a').on('click',function(){$('html, body').animate({scrollTop:0});});$win.on('scroll',function(){if($win.scrollTop()>0)$('.go-to-top').addClass('active');else $('.go-to-top').removeClass('active');}).on('load',function(){$win.trigger('scroll');});};
